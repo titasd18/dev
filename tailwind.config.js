a7a47ed7c8b10fd9fbf40b66aaab3dd24048895e
Shankhadeep Dey <shankhadeep@Flowrecs-Mac-mini.local>
2022-06-09 18:39:13 +0530

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        big: "48rem",
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    fontFamily: {
+      nunito: ["Nunito", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
